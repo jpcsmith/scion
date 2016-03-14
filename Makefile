7e065701c63d5c3678cb9b6f0b2fa45319d0470a
Jason Lee <jason.lee@inf.ethz.ch>
2016-03-14 14:38:12 +0100

    Rebase

--- Makefile
+++ Makefile
@@ -3,12 +3,9 @@
 CC=gcc
 CFLAGS +=-Wall -g
 LIB_DIR=lib/libscion
-LIBFILE=$(LIB_DIR)/libscion.a
-LIB_H_SRC=$(LIB_DIR)/*.c $(LIB_DIR)/*.h
 DISPATCHER_DIR=endhost
 DISPATCHER=$(DISPATCHER_DIR)/dispatcher
 SOCKET_DIR=endhost/ssp
-SOCKET=$(SOCKET_DIR)/libsocket.so
 
 all:
 	$(MAKE) -C $(LIB_DIR)
--- lib/libscion/packet.c
+++ lib/libscion/packet.c
@@ -7,6 +7,8 @@
 
 #include "scion.h"
 
+uint8_t L4PROTOCOLS[] = {L4_ICMP, L4_TCP, L4_UDP, L4_SSP};
+
 void build_cmn_hdr(uint8_t *buf, int src_type, int dst_type, int next_hdr)
 {
     SCIONCommonHeader *sch = (SCIONCommonHeader *)buf;
@@ -16,25 +18,25 @@ void build_cmn_hdr(uint8_t *buf, int src_type, int dst_type, int next_hdr)
     sch->versionSrcDst = htons(vsd);
     sch->nextHeader = next_hdr;
     sch->headerLen = sizeof(*sch);
-    sch->currentIOF = 0;
-    sch->currentOF = 0;
     sch->totalLen = htons(sch->headerLen);
 }
 
-void build_addr_hdr(uint8_t *buf, SCIONAddr *src, SCIONAddr *dst)
+void build_addr_hdr(uint8_t *buf, uint8_t *src, uint8_t *dst)
 {
     SCIONCommonHeader *sch = (SCIONCommonHeader *)buf;
     int src_len = get_src_len(buf);
     int dst_len = get_dst_len(buf);
     int pad = (SCION_ADDR_PAD - ((src_len + dst_len) % 8)) % 8;
     uint8_t *ptr = (uint8_t *)sch + sizeof(*sch);
-    *(uint32_t *)ptr = htonl(src->isd_ad);
+    SCIONAddr *src_addr = (SCIONAddr *)src;
+    SCIONAddr *dst_addr = (SCIONAddr *)dst;
+    *(uint32_t *)ptr = htonl(src_addr->isd_ad);
     ptr += 4;
-    memcpy(ptr, src->host_addr, src_len);
+    memcpy(ptr, src_addr->host_addr, src_len);
     ptr += src_len;
-    *(uint32_t *)ptr = htonl(dst->isd_ad);
+    *(uint32_t *)ptr = htonl(dst_addr->isd_ad);
     ptr += 4;
-    memcpy(ptr, dst->host_addr, dst_len);
+    memcpy(ptr, dst_addr->host_addr, dst_len);
     sch->headerLen += src_len + dst_len + 8 + pad;
     sch->totalLen = htons(sch->headerLen);
 }
